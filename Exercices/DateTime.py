e4d968a836c36b42c4f3e8733b99b8a9a4ab253d
laurentziudmm <laurentziudmm@gmail.com>
2020-02-08 17:51:36 +0200

    added date time

---
+++ Exercices/DateTime.py
@@ -0,0 +1,30 @@
+﻿# import datetime 
+# now = datetime.datetime.now() 
+
+import datetime
+now = datetime.datetime.now(datetime.timezone.utc)
+# from exipfind import htmltext
+# from first_script import (x)
+# import pytz
+# from datetime import datetime
+# now = datetime.utcnow().replace(tzinfo=pytz.utc)
+
+# # print ("Current date and time using str method of datetime object:" )
+
+# print str(now)  
+# print ("Current date and time using instance attributes:" )
+# print ("Current year: %d" % now.year )
+# print ("Current month: %d" % now.month) 
+# print ("Current day: %d" % now.day )
+# print ("Current hour: %d" % now.hour) 
+# print ("Current minute: %d" % now.minute ) 
+# print ("Current second: %d" % now.second )
+# print ("Current microsecond: %d" % now.microsecond )
+
+
+# print(htmltext)
+print (" \n Current date and time :") 
+print (now.strftime("   %Y-%m-%d %H:%M:%S"))
+
+
+
