07e8b011d5d75b78c1d2b23ef9da94361e873e45
laurentziudmm <laurentziudmm@gmail.com>
2020-02-08 18:24:59 +0200

    update arithmetics

--- Exercices/arithmetics.py
+++ Exercices/arithmetics.py
@@ -50,43 +50,43 @@ x = x << 3
 
 # Python Comparison Operators
 # Operator	Name	Example
-== Equal x == yy
-!= Not equal x != yy
->	Greater than	x > yy
-<	Less than	x < yy
->= Greater than or equal to x >= yy
-<= Less than or equal to x <= yy
+# == Equal x == yy
+# != Not equal x != yy
+# >	Greater than	x > yy
+# <	Less than	x < yy
+# >= Greater than or equal to x >= yy
+# <= Less than or equal to x <= yy
 
 # Python Logical Operators
 
-Operator	Description	Example
-
-and Returns True if both statements are true	x < 5 and x < 10
-or Returns True if one of the statements is true	x < 5 or x < 4
-not Reverse the result, returns False if the result is true not(x < 5 and x < 10)
-
-# Python Identity Operators
-
-Operator	Description	Example
-is Returns true if both variables are the same object	x is yy
-is not Returns true if both variables are not the same object	x is not yy
-
-# Python Membership Operators
-
-Membership operators are used to test if a sequence is presented in an object:
-
-Operator	Description	Example
-in Returns True if a sequence with the specified value is present in the object	x in y
-not in Returns True if a sequence with the specified value is not present in the object	x not in y
-
+# Operator	Description	Example
+#
+# and Returns True if both statements are true	x < 5 and x < 10
+# or Returns True if one of the statements is true	x < 5 or x < 4
+# not Reverse the result, returns False if the result is true not(x < 5 and x < 10)
+#
+# # Python Identity Operators
+#
+# Operator	Description	Example
+# is Returns true if both variables are the same object	x is yy
+# is not Returns true if both variables are not the same object	x is not yy
+#
+# # Python Membership Operators
+#
+# Membership operators are used to test if a sequence is presented in an object:
+#
+# Operator	Description	Example
+# in Returns True if a sequence with the specified value is present in the object	x in y
+# not in Returns True if a sequence with the specified value is not present in the object	x not in y
+#
 # Python Bitwise Operators
 
-Bitwise operators are used to compare(binary) numbers:
-
-Operator	Name	Description
-& 	AND	Sets each bit to 1 if both bits are 1
-|	OR	Sets each bit to 1 if one of two bits is 1
-^	XOR	Sets each bit to 1 if only one of two bits is 1
-~ 	NOT	Inverts all the bits
-<< Zero fill left shift Shift left by pushing zeros in from the right and let the leftmost bits fall off
->> Signed right shift Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+# Bitwise operators are used to compare(binary) numbers:
+#
+# Operator	Name	Description
+# & 	AND	Sets each bit to 1 if both bits are 1
+# |	OR	Sets each bit to 1 if one of two bits is 1
+# ^	XOR	Sets each bit to 1 if only one of two bits is 1
+# ~ 	NOT	Inverts all the bits
+# << Zero fill left shift Shift left by pushing zeros in from the right and let the leftmost bits fall off
+# >> Signed right shift Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
