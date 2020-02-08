626251d398d3f7f1a1b7dc3ba58c9b7ea33a3037
laurentziudmm <laurentziudmm@gmail.com>
2020-02-08 17:54:29 +0200

    added condition tools

---
+++ Exercices/condition.py
@@ -0,0 +1,12 @@
+#Condition
+
+name = 'j'
+
+if len(name) < 3:
+    print("Name must be at least 3 characters:")
+elif len(name) > 50:
+    print("Name must be a max 50 characters")
+else:
+    print(name)
+
+
