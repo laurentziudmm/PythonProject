39036d448ce4b2a00ab110d83ca52fc5a8451df3
laurentziudmm <laurentziudmm@gmail.com>
2020-02-10 15:22:41 +0200

    added strings

---
+++ Exercices/strings.py
@@ -0,0 +1,14 @@
+course = "Python's Course for beginners"
+course = 'Python Course for "beginners"'
+
+course = '''  
+Hi John,
+
+Here is our first email to you.
+
+thank you
+'''
+
+course = 'Python for Beginners'
+another = course[:]
+print(another)
---
+++ Exercices/strings3.py
@@ -0,0 +1,20 @@
+course = 'Python for Begginers'
+
+print(course)
+
+print(course.upper())
+
+print(course.lower())
+
+print(course.find('y'))
+
+print(course.replace('Begginers', 'Absolute Begginers'))
+
+print('Python' in course)
+
+print('python' in course)
+
+
+print(course.upper())
+
+
