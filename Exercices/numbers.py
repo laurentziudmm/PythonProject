ca1c1a98afc1e9cc0343fb5262d14a27dc2583cc
laurentziudmm <laurentziudmm@gmail.com>
2020-02-11 18:55:48 +0200

    added numbers

---
+++ Exercices/numbers.py
@@ -0,0 +1,13 @@
+numbers = [5, 2, 5, 2, 2] + [2, 2, 2, 2, 5]
+# numbers2 = [2, 2, 2, 2, 5]
+# numbers3 = [2, 2, 2, 2, 5]
+# numbers4 = [2, 2, 2, 2, 5]
+# numbers5 = [2, 2, 2, 2, 5]
+
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
+
+
